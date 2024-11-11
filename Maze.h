948ed195280444d5add81c1570d94003882d57f8
debian <tl@zasmlabs.com>
2024-11-11 22:16:45 +0530

    added maze.cpp

--- Maze.h
+++ Maze.h
@@ -2,12 +2,18 @@
 #define Maze_H
 
 #include <vector>
+#include "Room.h"
 
 class Maze
 {
 public:
     Maze();
-    virtual Maze* Clone() const;
+    virtual Maze *Clone() const;
+    Room *RoomNo(int n) const;
+    void AddRoom(Room *room);
+
+private:
+    std::vector<Room *> *mRoomVec;
 };
 
 #endif
