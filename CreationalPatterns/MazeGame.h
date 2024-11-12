080d631d1c9ae6a04b12e3333146d890f53fdb21
debian <tl@zasmlabs.com>
2024-11-13 00:15:25 +0530

    added mazeGame.h

---
+++ CreationalPatterns/MazeGame.h
@@ -0,0 +1,37 @@
+#ifndef MAZE_GAME_H
+#define MAZE_GAME_H
+
+#include "Maze.h"
+#include "Room.h"
+#include "Wall.h"
+#include "Door.h"
+
+class MazeFactory;
+
+class MazeGame
+{
+public:
+    Maze *CreateMaze();
+
+    Maze *CreateMaze(MazeFactory &factory);
+ 
+    virtual Maze *MakeMaze() const
+    {
+        return new Maze;
+    }
+
+    virtual Room *MakeRoom(int n) const
+    {
+        return new Room(n);
+    }
+    virtual Wall *MakeWall() const
+    {
+        return new Wall;
+    }
+    virtual Door *MakeDoor(Room *r1, Room *r2) const
+    {
+        return new Door(r1, r2);
+    }
+};
+
+#endif
