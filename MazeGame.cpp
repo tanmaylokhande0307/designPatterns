6e4bea289e646764d8bdf5f38baedbeff13a9377
debian <tl@zasmlabs.com>
2024-11-13 22:34:17 +0530

    factory method

--- MazeGame.cpp
+++ MazeGame.cpp
@@ -29,3 +29,29 @@ Maze *MazeGame::CreateMaze()
 
     return aMaze;
 }
+
+Maze *MazeGame::CreateMaze(MazeFactory &factory)
+{
+
+    std::cout << "MazeGame::CreateMaze(MazeFactory&)" << std::endl;
+
+    Maze *aMaze = factory.MakeMaze();
+    Room *r1 = factory.MakeRoom(1);
+    Room *r2 = factory.MakeRoom(2);
+    Door *aDoor = factory.MakeDoor(r1, r2);
+
+    aMaze->AddRoom(r1);
+    aMaze->AddRoom(r2);
+
+    r1->SetSide(North, factory.MakeWall());
+    r1->SetSide(East, aDoor);
+    r1->SetSide(South, factory.MakeWall());
+    r1->SetSide(West, factory.MakeWall());
+
+    r2->SetSide(North, factory.MakeWall());
+    r2->SetSide(East, factory.MakeWall());
+    r2->SetSide(South, factory.MakeWall());
+    r2->SetSide(West, aDoor);
+
+    return aMaze;
+}
