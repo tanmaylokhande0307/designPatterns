1e72cc94ca7a51815f120b713eac10e624ff4fae
debian <tl@zasmlabs.com>
2024-11-13 22:24:06 +0530

    added enchanted and bombed maze game and supporting classes

---
+++ CastSpell.h
@@ -0,0 +1,9 @@
+#ifndef CAST_SPELL_H
+#define CAST_SPELL_H
+#include "Spell.h"
+
+class CastSpell : public Spell
+{
+};
+
+#endif 
---
+++ EnchantedRoom.cpp
@@ -0,0 +1,8 @@
+
+#include "EnchantedRoom.h"
+#include "Spell.h"
+
+EnchantedRoom::EnchantedRoom(int n, Spell *spell)
+    : Room(n), _spell(spell)
+{
+}
