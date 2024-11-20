e0329e90d85bc883f0ceb54448e0cc4869666136
debian <tl@zasmlabs.com>
2024-11-20 23:21:46 +0530

    decorator pattern example

---
+++ StructuralPatterns/Decorator/Decorator.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+class Component
+{
+public:
+    virtual ~Component() {}
+
+    virtual void operation() = 0;
+};
+
+class ConcreteComponent : public Component
+{
+public:
+    ~ConcreteComponent() {}
+
+    void operation()
+    {
+        std::cout << "Concrete Component operation" << std::endl;
+    }
+};
+
+class Decorator : public Component
+{
+public:
+    ~Decorator() {}
+
+    Decorator(Component *c) : component(c) {}
+
+    virtual void operation()
+    {
+        component->operation();
+    }
+
+private:
+    Component *component;
+};
+
+class ConcreteDecoratorA : public Decorator
+{
+public:
+    ConcreteDecoratorA(Component *c) : Decorator(c) {}
+
+    void operation()
+    {
+        Decorator::operation();
+        std::cout << "Decorator A" << std::endl;
+    }
+};
+
+class ConcreteDecoratorB : public Decorator
+{
+public:
+    ConcreteDecoratorB(Component *c) : Decorator(c) {}
+
+    void operation()
+    {
+        Decorator::operation();
+        std::cout << "Decorator B" << std::endl;
+    }
+};
+
+int main()
+{
+    ConcreteComponent *cc = new ConcreteComponent();
+    ConcreteDecoratorB *db = new ConcreteDecoratorB(cc);
+    ConcreteDecoratorA *da = new ConcreteDecoratorA(db);
+
+    Component *component = da;
+    component->operation();
+
+    delete da;
+    delete db;
+    delete cc;
+
+    return 0;
+}
