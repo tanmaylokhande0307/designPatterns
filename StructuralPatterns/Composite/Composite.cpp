9e35a3a57fc74dc7d5cb409a601999f84394e2ab
debian <tl@zasmlabs.com>
2024-11-20 21:43:12 +0530

    sample code

---
+++ StructuralPatterns/Composite/Composite.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+
+class Component
+{
+public:
+    virtual Component *getChild(int)
+    {
+        return 0;
+    }
+
+    virtual void add(Component *) {};
+    virtual void remove(int) {};
+
+    virtual void operation() = 0;
+};
+
+class Composite : public Component
+{
+
+public:
+    ~Composite()
+    {
+        for (unsigned int i = 0; i < children.size(); i++)
+        {
+            delete children[i];
+        }
+    }
+
+    Component *getChild(const unsigned int index)
+    {
+        return children[index];
+    }
+
+    void add(Component *component)
+    {
+        children.push_back(component);
+    }
+
+    void remove(const unsigned int index)
+    {
+        Component *child = children[index];
+        children.erase(children.begin() + index);
+        delete child;
+    }
+
+    void operation()
+    {
+        for (unsigned int i = 0; i < children.size(); i++)
+        {
+            children[i]->operation();
+        }
+    }
+
+private:
+    std::vector<Component *> children;
+};
+
+class Leaf : public Component
+{
+public:
+    Leaf(const int i) : id(i) {}
+
+    ~Leaf() {}
+
+    void operation()
+    {
+        std::cout << "Leaf " << id << " operation" << std::endl;
+    }
+
+private:
+    int id;
+};
+
+int main()
+{
+    Composite composite;
+
+    for (unsigned int i = 0; i < 5; i++)
+    {
+        composite.add(new Leaf(i));
+    }
+
+    composite.remove(0);
+    composite.operation();
+
+    return 0;
+}
